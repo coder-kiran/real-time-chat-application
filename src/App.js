74c7aad6395eceb26a19f05bf4e86e92a464e991
Kiran K K <kirankalak4@gmail.com>
2021-07-06 20:29:50 +0530

    added components to customize the chat feed at middle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { ChatEngine } from 'react-chat-engine'
 import './App.css'
-
+import ChatFeed from './components/ChatFeed'
 function App(){
     return(
         <ChatEngine
@@ -8,6 +8,7 @@ function App(){
         projectID="ff13b3bf-725d-4cea-b852-8da0a1ccd15a"
         userName="kiran"
         userSecret="1234abcd@"
+        renderChatFeed={ (chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     )
 }
